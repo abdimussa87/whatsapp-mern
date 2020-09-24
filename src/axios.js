14e7278e8470f0fd9a47358c4cf91d7bf6b3d4a2
abdi mussa <abdimussa87@gmail.com>
2020-09-24 13:56:00 +0300

    Changed the base url of axios

--- src/axios.js
+++ src/axios.js
@@ -1,4 +1,4 @@
 import axios from "axios";
 
-const instance = axios.create({baseURL:'http://localhost:9000'});
+const instance = axios.create({baseURL:'https://still-headland-39753.herokuapp.com'});
 export default instance;
